6d2bbdb7bff9723785bf4ef9a22414cf4df785a9
EliseGabilly <elise.gabilly@gmail.com>
2021-11-18 21:55:00 +0100

    Recuperation projet de base

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,11 @@
-
+import org.eclipse.swt.widgets.Shell;
 
 public class Main {
     public static void main(String[] args) {
         System.out.print("Hello world!");
+
+        Shell shell = new Shell();
+        PageChargement laPageDeCharge = new PageChargement(shell);
+
     }
 }
